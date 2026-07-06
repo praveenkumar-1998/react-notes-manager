d39f55949dbb808e5f38d484a03ed3c553fc33a9
praveenkumar-1998 <tpk6123@gmail.com>
2026-07-06 15:49:36 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-notes-app/' // MUST match repo name
+  base: '/react-notes-manager/' // MUST match repo name
 })
